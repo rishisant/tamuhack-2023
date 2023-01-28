c868e327bf34cf61a6532955b7623fe9ad72ca11
Matthew Juntunen <matthewjunt@tamu.edu>
2023-01-28 13:46:14 -0600

    added config for authentication

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,31 @@ import largeLogo from '../img/mmlogo_2.png';
 
 import '../BaseStyle.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKHYrPdMSFZ-o6AfWwpPEUd8NPYDMnn6k",
+  authDomain: "musclemates-a7c0b.firebaseapp.com",
+  projectId: "musclemates-a7c0b",
+  storageBucket: "musclemates-a7c0b.appspot.com",
+  messagingSenderId: "481952527135",
+  appId: "1:481952527135:web:c1a7ea2c1b4555c853362a",
+  measurementId: "G-L3B80R35L1"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
 // display as flex row with values "login", "signup", "about", "profile", with logo in the side
 const tpBarStyle = {
     display: 'flex',
