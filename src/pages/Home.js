470edbb9baabf76de98c3351958563bd990977ae
Rishi Santhanam <rishisant@tamu.edu>
2023-01-28 12:56:40 -0600

    pushing first

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+/*
+author: @rishisant @matthewjunt
+*/
+
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
