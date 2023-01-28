a973e0146fb170d78f0e886613e6bbc86f020ea5
Rishi Santhanam <rishisant@tamu.edu>
2023-01-28 13:09:11 -0600

    changed main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,20 @@ author: @rishisant @matthewjunt
 
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import smallLogo from '../img/mmlogo_1.png';
+import largeLogo from '../img/mmlogo_2.png';
+
+import '../BaseStyle.css';
+
 
 const Home = () => {
     return (
         <div>
-        <h1>Home</h1>
+            <body>
+                <div className="logoContainer">
+                    <img id="sLogo" src={smallLogo} alt="small logo" className="smallLogo" />
+                </div>
+            </body>
         </div>
     );
 }
