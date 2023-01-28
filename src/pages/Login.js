27d32ce53b62b7387c5887535d35669ffc2bdd2b
Rishi Santhanam <rishisant@tamu.edu>
2023-01-28 14:32:22 -0600

    hihi

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,48 +6,23 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import smallLogo from '../img/mmlogo_1.png';
 import largeLogo from '../img/mmlogo_2.png';
+import { tpBarStyle } from './Home';
+import { menuItemStyle } from './Home';
 
 import '../BaseStyle.css';
 
+// const tpBarStyle = {
+//     display: 'flex',
+//     flexDirection: 'row',
+// }
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// const menuItemStyle = {
+//     fontSize: '2em',
+//     padding: '1em',
+//     margin: '1em',
+// }
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCKHYrPdMSFZ-o6AfWwpPEUd8NPYDMnn6k",
-  authDomain: "musclemates-a7c0b.firebaseapp.com",
-  projectId: "musclemates-a7c0b",
-  storageBucket: "musclemates-a7c0b.appspot.com",
-  messagingSenderId: "481952527135",
-  appId: "1:481952527135:web:c1a7ea2c1b4555c853362a",
-  measurementId: "G-L3B80R35L1"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// display as flex row with values "login", "signup", "about", "profile", with logo in the side
-export const tpBarStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-}
-
-export const menuItemStyle = {
-    fontSize: '2em',
-    padding: '1em',
-    margin: '1em',
-}
-
-
-const Home = () => {
+const Login = () => {
     const navigate = useNavigate();
 
     return (
@@ -83,4 +58,4 @@ const Home = () => {
     );
 }
 
-export default Home;
+export default Login;
