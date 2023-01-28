0f9cf4c12a977cc7ad0504e612eafa3970707752
Rishi Santhanam <rishisant@tamu.edu>
2023-01-28 13:29:30 -0600

    pushing for

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,38 @@ import largeLogo from '../img/mmlogo_2.png';
 
 import '../BaseStyle.css';
 
+// display as flex row with values "login", "signup", "about", "profile", with logo in the side
+const tpBarStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+
+}
+
+const menuItemStyle = {
+    fontSize: '2em',
+    padding: '1em',
+    margin: '1em',
+}
 
 const Home = () => {
     return (
         <div>
             <body>
-                <div className="logoContainer">
-                    <img id="sLogo" src={smallLogo} alt="small logo" className="smallLogo" />
+                <div className="topmenuBar" style={tpBarStyle}>
+                    <div className="logoContainer">
+                        <img src={largeLogo} alt="large logo" className="largeLogo" />
+                    </div>
+                    <div className="menuButton">
+                        Login
+                    </div>
+                    <div className="menuButton">
+                        Sign Up
+                    </div>
+                    <div className="menuButton">
+                        About
+                    </div>
                 </div>
+                
             </body>
         </div>
     );
